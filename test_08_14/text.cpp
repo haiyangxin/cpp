02fe20a9a8e04c79bedb7b85e8869c163c3e3870
haiyangxin <2791317893@qq.com>
2023-08-14 22:38:09 +0800

    init a file

---
+++ test_08_14/text.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "max.h"//引用自己的头文件
+using namespace std;
+int main()
+{
+    int a=10;
+int b=20;
+int c=Max(a,b);
+    cout<<"Hello,World."<<endl;
+    cout<<c;
+    return 0;
+}
